f0c35a9542e36617408984222e4c65c13696bea5
Aster-Sider <127595994+Aster-Sider@users.noreply.github.com>
2023-07-04 12:12:04 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+alert('Hello World!');
+
+// Завдання 1
+let city = 'Kiev';
+city = 'Lviv';
+let address = city
+alert (address);
+
+// Завдання 2
+const n = 7;
+let result = n**3;
+console.log(result);
+
+// Завдання 3
+let pen = 4;
+let pencil = 6;
+const calculation = (pen * 5.25) + (pencil * 3.25) 
+console.log("Together " + calculation)
+
